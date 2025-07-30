dbcdbb56b9eee49c74c280635d019e1c0530c22e
Lucas Eduardo <lucased18@gmail.com>
2025-07-30 17:26:11 -0300

    projeto criado

---
+++ project-root/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, HomeComponent, NavbarComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'project-root';
+}
