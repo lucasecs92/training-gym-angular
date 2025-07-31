9c41b3b17dfe3c86c6582fc9d88a7a0193c5ba3e
Lucas Eduardo <102336205+lucasecs92@users.noreply.github.com>
2025-07-31 00:03:16 -0300

    feat
    componentes home criados

--- project-root/src/app/app.component.ts
+++ project-root/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ArticlesComponent } from './components/articles/articles.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, NavbarComponent],
+  imports: [RouterOutlet, HeaderComponent, NavbarComponent, ArticlesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ project-root/src/app/components/articles/articles.component.ts
@@ -0,0 +1,18 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-articles',
+  standalone: true,
+  imports: [
+    NgOptimizedImage
+  ],
+  templateUrl: './articles.component.html',
+  styleUrl: './articles.component.css'
+})
+export class ArticlesComponent {
+  gymOneUrl: string = '../../../assets/images/gym-01.png';
+  gymAltText: string = 'Imagem de uma academia com equipamentos modernos e pessoas treinando.';
+  gymTwoUrl: string = '../../../assets/images/gym-02.png';
+  gymTwoAltText: string = 'Imagem de uma academia com pessoas treinando e equipamentos';
+}
