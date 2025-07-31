ec444d5991488f48bf2b86d3f24ef30400845bae
Lucas Eduardo <102336205+lucasecs92@users.noreply.github.com>
2025-07-30 21:33:33 -0300

    feat
    componente navbar criado

--- project-root/src/app/components/navbar/navbar.component.ts
+++ project-root/src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,16 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true, // Add 'standalone: true' pois o seu 'imports' estava vazio, indicando que é um componente standalone.
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+  logoUrl: string = '../../../assets/images/logo-icon.svg';
+  logoAltText: string = 'Logo Training GYN';
 }
