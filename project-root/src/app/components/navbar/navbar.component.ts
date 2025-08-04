1cc32bd80621b6e91781d6929f7f7b677ac52a1a
Lucas Eduardo <lucased18@gmail.com>
2025-08-04 15:20:44 -0300

    feat
    ajustes responsividade navbar

--- project-root/src/app/components/navbar/navbar.component.ts
+++ project-root/src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,11 @@
-import { NgOptimizedImage } from '@angular/common';
+import { NgIf, NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true, // Add 'standalone: true' pois o seu 'imports' estava vazio, indicando que é um componente standalone.
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage, NgIf
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
@@ -13,4 +13,12 @@ import { Component } from '@angular/core';
 export class NavbarComponent {
   logoUrl: string = '../../../assets/images/logo-icon.svg';
   logoAltText: string = 'Logo Training GYN';
+
+  // propriedade para controlar a visibilidade do menu
+  isMenuOpen = false;
+
+  // Função para alternar o estado do menu
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
