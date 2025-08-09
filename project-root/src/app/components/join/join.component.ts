62e95ef04d91920fdef221b87bba00c2aa5d2e34
Lucas Eduardo <lucased18@gmail.com>
2025-08-09 10:05:37 -0300

    feat
    add router reserve

--- project-root/src/app/components/join/join.component.ts
+++ project-root/src/app/components/join/join.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-join',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './join.component.html',
   styleUrl: './join.component.css'
 })
