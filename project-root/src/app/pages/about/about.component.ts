6d9c4aa3b0a51d05f5fdc7ea50f44f73d23f8a14
Lucas Eduardo <lucased18@gmail.com>
2025-08-09 11:13:08 -0300

    feat
    component about em andamento

--- project-root/src/app/pages/about/about.component.ts
+++ project-root/src/app/pages/about/about.component.ts
@@ -1,11 +1,16 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  standalone: true,
+  imports: [NgOptimizedImage],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
-
+  gymAboutOneUrl: string = '../../../assets/images/gym-about-01.png';
+  gymAboutAltText: string = 'Imagem de uma academia com equipamentos modernos e pessoas treinando.';
+  gymAboutTwoUrl: string = '../../../assets/images/gym-about-02.png';
+  gymAboutThreeUrl: string = '../../../assets/images/gym-about-03.png';
 }
